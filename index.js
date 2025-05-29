a377f0d8ae118055730d24cc26960fc35b789f34
Vishali Sharma <vishalisharma964@gmail.com>
2025-05-29 23:38:25 +0530

    Update index.js

--- index.js
+++ index.js
@@ -22,11 +22,11 @@ const app =express();
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(cookieParser());
-const corsOptions ={
-    origin:'http://localhost:5173',
-    credentials:true
-}
-app.use(cors(corsOptions));
+app.use(cors({
+    origin:[process.env.FRONTEND_URL],
+    methods:["POST","GET","PUT","DELETE"],
+    credentials:true,
+}));
 const PORT = process.env.PORT || 3000;
 //api's
 app.use("/api/v1/user",userRoute);
